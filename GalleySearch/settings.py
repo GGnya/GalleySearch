27a32bc0b6afad8319303de54b14ddee2e5ec3a0
GGnya <jenia.barykin@gmail.com>
2023-03-03 18:02:13 +0300

    changed settings

--- GalleySearch/settings.py
+++ GalleySearch/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = BASE_DIR
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static/"]
 
 # Default primary key field type
